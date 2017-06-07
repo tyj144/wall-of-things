20e706ce400a5231f2c8cf6b065df7d5e9ce721c
tyj144 <tylerj81@gmail.com>
2017-06-07 16:02:10 -0400

    Added templates and dynamic content

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    number = 4
+    thing = "Thing 1"
+    return render(request, 'index.html', {
+        'number': number,
+        'thing': thing,
+    })
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
+from django.views.generic import TemplateView
 from collection import views
 
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls), 
     url(r'^$', views.index, name='home'), 
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ]
