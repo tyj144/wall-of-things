f37ad9e5dffddde52b7e66e90758a3f6c4a0cd65
tyj144 <tylerj81@gmail.com>
2017-06-07 16:40:58 -0400

    Added admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+# importing model
+from collection.models import Thing
+
+# set up automated slug creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_displays = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
 # Register your models here.
+admin.site.register(Thing, ThingAdmin)
